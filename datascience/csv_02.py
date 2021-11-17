1cc717c559aebd0d8ad1d8ff5b03704abe0f166a
Will Holton <willholtonjtb@gmail.com>
2021-11-17 19:04:49 +0000

    minor edit to add header row

--- datascience/csv_02.py
+++ datascience/csv_02.py
@@ -9,7 +9,7 @@ path_to_new_data = os.path.join(base_directory,"output_no_git","test_images.csv"
 number_required = 100
 number_processed = 0
 with open(path_to_new_data, "w") as file:
-    pass
+    file.write("uniq_id, image_url\n")
 for line_index,line in enumerate(lines):
     entries = line.split(',')
     unique_id = entries[0]
